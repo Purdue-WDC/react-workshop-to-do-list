399d714641663e74308e2b6e6300d6a73d9271dd
Shayne Marques <marques0@purdue.edu>
2021-02-27 22:33:17 -0500

    Added delete button and completed inteface

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,5 +1,6 @@
-import "./App.css";
 import React from "react";
+
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,15 +11,45 @@ class App extends React.Component {
     };
     this.handleAddButton = this.handleAddButton.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
+    this.toggleCheckboxChange = this.toggleCheckboxChange.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
-
+  toggleCheckboxChange(event) {
+    const listCopy = this.state.list;
+    console.log(listCopy);
+    const foundIndex = this.state.list.findIndex(
+      (item) => item.name === event.target.name
+    );
+    console.log(foundIndex);
+    const isChecked = this.state.list[foundIndex].complete;
+    console.log(isChecked);
+    listCopy.splice(foundIndex, 1, {
+      name: this.state.list[foundIndex].name,
+      complete: !this.state.list[foundIndex].complete,
+    });
+    console.log(isChecked);
+    this.setState((state) => ({
+      list: [...listCopy],
+      value: state.value,
+    }));
+  }
   handleTextChange(event) {
     this.setState((state) => ({
       list: [...state.list],
       value: event.target.value,
     }));
   }
-
+  handleDelete(event) {
+    const listCopy = this.state.list;
+    const foundIndex = this.state.list.findIndex(
+      (item) => item.name === event.target.name
+    );
+    listCopy.splice(foundIndex, 1);
+    this.setState((state) => ({
+      list: [...listCopy],
+      value: state.value,
+    }));
+  }
   handleAddButton(event) {
     if (this.state.value !== "") {
       this.setState((state) => ({
@@ -26,9 +57,8 @@ class App extends React.Component {
         value: "",
       }));
     }
-    console.log(this.state.list);
+    document.getElementById("task").value = "";
   }
-
   render() {
     // Any elements that need to be returned must be in a wrapper element like a <div> </div>
     return (
@@ -42,6 +72,7 @@ class App extends React.Component {
             <input
               name="task-input"
               type="text"
+              id="task"
               className="text-input"
               placeholder="Enter your next task..."
               onChange={this.handleTextChange}
@@ -53,9 +84,61 @@ class App extends React.Component {
           <div className="grid">
             <div className="active">
               <h1>Active</h1>
+              <ul>
+                {this.state.list.map((obj) => {
+                  if (!obj.complete) {
+                    return (
+                      <li>
+                        <input
+                          type="checkbox"
+                          name={obj.name}
+                          checked={obj.complete}
+                          onChange={this.toggleCheckboxChange}
+                        />
+                        <label htmlFor={obj.name}>{obj.name}</label>
+                        <button
+                          id={obj.name}
+                          onClick={this.handleDelete}
+                          className="del-button"
+                        >
+                          Del
+                        </button>
+                      </li>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+              </ul>
             </div>
             <div className="completed">
               <h1>Completed</h1>
+              <ul>
+                {this.state.list.map((obj) => {
+                  if (obj.complete) {
+                    return (
+                      <li>
+                        <input
+                          type="checkbox"
+                          name={obj.name}
+                          checked={obj.complete}
+                          onChange={this.toggleCheckboxChange}
+                        />
+                        <label htmlFor={obj.name}>{obj.name}</label>
+                        <button
+                          id={obj.name}
+                          onClick={this.handleDelete}
+                          className="del-button"
+                        >
+                          Del
+                        </button>
+                      </li>
+                    );
+                  } else {
+                    return null;
+                  }
+                })}
+              </ul>
             </div>
           </div>
         </main>
@@ -63,14 +146,5 @@ class App extends React.Component {
     );
   }
 }
-
-/*
-  You can create functional components as well
-  
-  function App() {
-
-  }
-  
-*/
 
 export default App;
